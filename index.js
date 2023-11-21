b08dc484ba8a6378c8b26b946949b026e6b8393d
Erik De Rijcke <erik.derijcke@cegeka.com>
2023-11-21 21:56:37 +0100

    simplify to simple dynamic library loading

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const textDecoder = new TextDecoder()
 
 async function main() {
     const memory = new WebAssembly.Memory({initial: 1024, maximum: 10240})
-    const loader = new Loader(memory, { println: (messagePtr) => {
+    const println = (messagePtr) => {
         const uint8Memory = new Uint8Array(memory.buffer);
         let size = 0;
         while(uint8Memory[messagePtr+size] !== 0) {
@@ -12,7 +12,9 @@ async function main() {
         }
         const message = uint8Memory.subarray(messagePtr, messagePtr+size);
         console.log(textDecoder.decode(message))
-    }})
+    }
+
+    const loader = new Loader(memory, { println })
     const instance = await loader.load('toy_app.wasm')
     if (instance.exports.main) {
         instance.exports.main()
